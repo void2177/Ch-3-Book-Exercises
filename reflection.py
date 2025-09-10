f934c2d38b09c8da2c4251894830605be68e7592
carterbrody <clb2377@gmail.com>
2025-09-10 16:47:15 -0700

    Finished chapter exercises

---
+++ reflection.py
@@ -0,0 +1,20 @@
+## For exercise 1, there wasn't much way to deviate because it was asking us to
+##investigate, not find a solution.
+
+##For exercise 2, I definitely think I found an easier solution than the instructor. My code took
+##only 3 lines of code, depending on how many lines of text you wanted. I can see how the
+##instructor's code can be improved, mainly by reducing the complexity.
+
+##For exercise 3, I really struggled with this and had to ask ChatGPT for help often. The
+##instructor had a better script written for this, mine wasn't that good.
+
+##For exercise 4, I thought the problem was relatively straightforward. I think it matches
+##exercise 2, where the instructor's solution was unnecessarily complicated. It only took
+##a couple lines of code for me personally.
+
+##With exercise 5, I believe the instructor wrote this better. I kinda struggled with "for" loops,
+##and so while I did complete exercise 5, there's one line in the output where it's
+##grammatically incorrect, saying "bottles" instead of "bottle".
+
+##With all solutions, I can't find a better solution other than the one that was arguably better.
+##Neither could be made better than the superior solution already was.
